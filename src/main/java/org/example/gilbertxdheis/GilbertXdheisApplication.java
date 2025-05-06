45e459207e555fb1b6b2248f6a41f0d96c3c2d28
Daniel <dmastek@hotmail.com>
2025-05-06 11:56:37 +0200

    Initial commit

---
+++ src/main/java/org/example/gilbertxdheis/GilbertXdheisApplication.java
@@ -0,0 +1,13 @@
+package org.example.gilbertxdheis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GilbertXdheisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GilbertXdheisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gilbertxdheis/GilbertXdheisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gilbertxdheis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GilbertXdheisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
