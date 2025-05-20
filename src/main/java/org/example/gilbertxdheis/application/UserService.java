a87fbe2cb068bf7cbf65c66c8dbf43d7c9cf549a
TjNowOnline <dmastek@hotmail.com>
2025-05-20 16:15:04 +0200

    Encryption of password fix

--- src/main/java/org/example/gilbertxdheis/application/UserService.java
+++ src/main/java/org/example/gilbertxdheis/application/UserService.java
@@ -2,18 +2,26 @@ package org.example.gilbertxdheis.application;
 
 import org.example.gilbertxdheis.domain.User;
 import org.example.gilbertxdheis.infrastructure.JdbcUserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final JdbcUserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(JdbcUserRepository userRepository) {
+    public UserService(JdbcUserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void createUser(User user) {
         System.out.println("createUser method called");
+
+        // Encrypt the password
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
         userRepository.create(user);
         System.out.println(user);
     }
