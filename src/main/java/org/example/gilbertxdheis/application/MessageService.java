6baaf1d6d0988bb270fff5d04b1ca0fd8e5a4701
Daniel <dmastek@hotmail.com>
2025-06-12 09:59:47 +0200

    Billeder nye

--- src/main/java/org/example/gilbertxdheis/application/MessageService.java
+++ src/main/java/org/example/gilbertxdheis/application/MessageService.java
@@ -1,21 +1,21 @@
-package org.example.gilbertxdheis.application;
-import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
-import org.example.gilbertxdheis.infrastructure.EmailService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class MessageService {
-    public void messageService(String recepient, String subject, String message) throws
-            Exception {
-        try {
-           EmailService.MailSystem.sendmail(recepient, subject, message);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-}
+//package org.example.gilbertxdheis.application;
+//import com.twilio.Twilio;
+//import com.twilio.rest.api.v2010.account.Message;
+//import com.twilio.type.PhoneNumber;
+//import org.example.gilbertxdheis.infrastructure.EmailService;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class MessageService {
+//    public void messageService(String recepient, String subject, String message) throws
+//            Exception {
+//        try {
+//           EmailService.MailSystem.sendmail(recepient, subject, message);
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//}
 
 
 
